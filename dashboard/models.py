9add32a8e47958e4136f6a87662c9163f605c0af
wakka <neupaneshreeyesh@gmail.com>
2023-06-18 06:45:20 +0545

    First Commit

---
+++ dashboard/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+CATEGORY=(
+    ('Stationary','Stationary'),
+    ('Elctronics','Electronics'),
+    ('Food','Food'),
+)
+
+class Product(models.Model):
+    name=models.CharField(max_length=100, null=True)
+    category=models.CharField(max_length=20,choices=CATEGORY, null=True)
+    quantity=models.PositiveBigIntegerField()
+
+    class Meta:
+        verbose_name_plural='Product'
+
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'
+    
+class Order(models.Model):
+    Product=models.ForeignKey(Product,on_delete=models.CASCADE,null=True)
+    staff=models.ForeignKey(User,models.CASCADE,null=True)
+    order_quantity=models.PositiveIntegerField(null=True)
+    date=models.DateTimeField(auto_now_add=True)
+
+    
+    class Meta:
+        verbose_name_plural='Order'
+
+    def __str__(self):
+        return f'{self.Product} ordered by {self.staff.username}'
